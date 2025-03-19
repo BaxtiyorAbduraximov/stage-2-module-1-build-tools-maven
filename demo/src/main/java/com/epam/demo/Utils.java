5455a6f4828bc44c1c72837a61c956112299f483
Bakhtiyor22 <baxtiyorabduraximov2203@gmail.com>
2025-03-19 23:11:47 +0500

    commit2

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,11 +1,11 @@
 package com.epam.demo;
 
-import org.apache.commons.lang3.StringUtils;
+import com.epam.utils.StringUtils;
 
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().allMatch(StringUtils::isAllBlank);
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
